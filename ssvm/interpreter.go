363e226933e8c0412c154d7c109f6dda453882c9
reraru505 <fnln504@gmail.com>
2023-02-26 20:58:20 +0530

    The VM is now turing complete , FUCK YEAH!!!!!!!!

--- ssvm/interpreter.go
+++ ssvm/interpreter.go
@@ -13,6 +13,13 @@ func interpreter(code []string){
 	vm.proccollector()
 	vm.labelcollector()
 
+	for _ , i := range vm.procarr{
+		if "main" == i.name{
+			vm.codeindex = i.index
+			break
+		}
+	}
+
 	for{
 		
 		if islabel(vm.code[vm.codeindex]){
@@ -31,10 +38,26 @@ func interpreter(code []string){
 			
 		}else if vm.code[vm.codeindex] == "call"{
 			vm.call(vm.code[vm.codeindex+1])
-			vm.codeindex++
+			//vm.codeindex++
 			
 		}else if vm.code[vm.codeindex] == "ret"{
 			vm.ret()
+			//fmt.Println("<debug> returned at ",vm.code[vm.codeindex])
+			//vm.codeindex++
+		}else if vm.code[vm.codeindex] == "lda"{
+			vm.lda(vm.code[vm.codeindex+1])
+			vm.codeindex++
+			
+		}else if vm.code[vm.codeindex] == "ldx"{
+			vm.ldx(vm.code[vm.codeindex+1])
+			vm.codeindex++
+			
+		}else if vm.code[vm.codeindex] == "ldy"{
+			vm.ldy(vm.code[vm.codeindex+1])
+			vm.codeindex++
+			
+		}else if vm.code[vm.codeindex] == "inc"{
+			vm.inc(vm.code[vm.codeindex+1])
 			vm.codeindex++
 			
 		}else if vm.code[vm.codeindex] == "add"{
@@ -63,7 +86,15 @@ func interpreter(code []string){
 			
 		}else if vm.code[vm.codeindex] == "je"{
 			vm.je(vm.code[vm.codeindex+1])
-			vm.codeindex++
+			
+		}else if vm.code[vm.codeindex] == "jne"{
+			vm.jne(vm.code[vm.codeindex+1])
+			
+		}else if vm.code[vm.codeindex] == "jig"{
+			vm.jig(vm.code[vm.codeindex+1])
+			
+		}else if vm.code[vm.codeindex] == "jil"{
+			vm.jil(vm.code[vm.codeindex+1])
 			
 		}else if vm.code[vm.codeindex] == "print"{
 			vm.print(vm.code[vm.codeindex+1])
--- ssvm/ssvm.go
+++ ssvm/ssvm.go
@@ -5,6 +5,7 @@ import(
 	"strings"
 	"io/ioutil"
 	"os"
+	"strconv"
 )
 
 func fileload(src string) []string{
@@ -32,12 +33,12 @@ type label struct {
 type machine struct{
 
 	//registers
-	A interface{}
-	X interface{}
-	Y interface{}
+	A string
+	X string
+	Y string
 
 	//stack
-	stack []interface{}
+	stack [1024*1024]string
 	top int
 	bottom int
 
@@ -54,7 +55,9 @@ type machine struct{
 	
 	//flags
 	carry    bool
-	cmpf      bool
+	cmpf     bool
+	greater  bool
+	lesser   bool
 	overflow bool
 	negative bool
 	
@@ -67,8 +70,14 @@ func (m * machine) proccollector() {
 	for j , i := range m.code{
 		mb := []rune(i)
 		if(mb[0] == '.'){
+			var name []rune
+			for _ , k := range mb {
+				if k != '.'{
+					name = append(name,k)
+				}
+			}
 			var buffer proc
-			buffer.name = i
+			buffer.name = string(name)
 			buffer.index = j
 
 			m.procarr = append(m.procarr,buffer)
@@ -81,9 +90,15 @@ func (m * machine) labelcollector() {
 
 	for j , i := range m.code{
 		mb := []rune(i)
-		if(mb[0] == '_'){
+		if(mb[0] == '#'){
+			var name []rune
+			for _ , k := range mb {
+				if k != '#'{
+					name = append(name,k)
+				}
+			}
 			var buffer label
-			buffer.name = i
+			buffer.name = string(name)
 			buffer.index = j
 
 			m.labelarr = append(m.labelarr,buffer)
@@ -91,21 +106,77 @@ func (m * machine) labelcollector() {
 	}
 }
 
+func (m *machine) lda(val string){
+	if val == "[X]"{
+		m.A = m.X
+	}else if val == "[Y]"{
+		m.A = m.Y
+	}else{
+		m.A = val
+	}
+
+	m.codeindex++
+}
 
 
-func (m *machine) push(val interface{}){
-	m.stack = append(m.stack,val)
+func (m *machine) ldx(val string){
+	if val == "[A]"{
+		m.X = m.A
+	}else if val == "[Y]"{
+		m.X = m.Y
+	}else{
+		m.X = val
+	}
+	m.codeindex++
+}
+
+
+func (m *machine) ldy(val string){
+	if val == "[X]"{
+		m.Y = m.X
+	}else if val == "[A]"{
+		m.Y = m.A
+	}else{
+		m.Y = val
+	}
+	m.codeindex++
+}
+
+func (m *machine) inc (val string){
+
+	if val == "[X]"{
+		i,_ := strconv.Atoi(m.X)
+		i++
+		m.X = strconv.Itoa(i)
+	}else if val == "[A]"{
+		i,_ := strconv.Atoi(m.A)
+		i++
+		m.A = strconv.Itoa(i)
+	}else if val == "[Y]"{
+		i,_ := strconv.Atoi(m.Y)
+		//fmt.Println("<debug> value of Y = ",i)
+		i++
+		m.Y = strconv.Itoa(i)
+	}else{
+		fmt.Println("<debug> feture incomplete")
+	}
+	m.codeindex++
+}
+
+
+
+func (m *machine) push(val string){
+	m.stack[m.top] = val
 	m.top++
 	m.codeindex++
 }
 
-func (m *machine) pop() interface{} {
+func (m *machine) pop() string{
 	if m.top == 0 {
-		return 0
+		return "0"
 	}
-	a := m.stack[m.top]
 	m.top--
-	return a
+	return m.stack[m.top]
 }
 
 func (m *machine) call(val string){
@@ -115,9 +186,8 @@ func (m *machine) call(val string){
 	m.push(m.A)
 	m.push(m.X)
 	m.push(m.Y)
-	m.push(m.codeindex)
 
-	m.mainindex = m.codeindex+1
+	m.mainindex = m.codeindex-1
 	
 	for _ , i := range m.procarr{
 		if val == i.name{
@@ -133,66 +203,141 @@ func (m *machine) ret(){
 	m.Y = m.pop()
 	m.X = m.pop()
 	m.A = m.pop()
-
-	m.codeindex = m.mainindex
+	m.bottom = m.top
+	m.codeindex = m.mainindex 
 		
 }
 
-func (m *machine) add(a interface{},b interface{}){
-	c := a.(int)
-	d := b.(int)
-
-	m.A = c + d
-	m.codeindex += 2
-}
-
-func (m *machine) sub(a interface{},b interface{}){
-	c := a.(int)
-	d := b.(int)
-
-	m.A = c - d
-
-	m.codeindex += 2
-}
-
-func (m *machine) mul(a interface{} ,b interface{}){
-	c := a.(int)
-	d := b.(int)
-
-	m.A = c * d
-
-	m.codeindex += 2
-}
-
-func (m *machine) div(a interface{} ,b interface{}){
-	c := a.(int)
-	d := b.(int)
-
-	m.A = c / d
-
-	m.codeindex += 2
-}
-
-func (m *machine) mod(a interface{} ,b interface{}){
-	c := a.(int)
-	d := b.(int)
-
-	m.A = c % d
-
-	m.codeindex += 2
-}
-
-func (m *machine) cmp(a interface{},b interface{}){
-	if a == b {
-		m.cmpf = true
-	}else{
-		m.cmpf = false
+func (m *machine) add(a string,b string){
+	var av string
+	var bv string
+	if a == "[X]"{
+		av = m.X
+	}else if a == "[Y]"{
+		av = m.Y
+	}else if a == "[A]"{
+		av = m.A
+	}else {
+		av = a
 	}
 
+	if b == "[X]"{
+		bv = m.X
+	}else if b == "[Y]"{
+		bv = m.Y
+	}else if b == "[A]"{
+		bv = m.A
+	}else {
+		bv = b
+	}
+
+	c,_ := strconv.Atoi(av)
+	
+
+	d,_ := strconv.Atoi(bv)
+
+	var e int
+
+	// fmt.Println ("c ",c ,"and ","d ",d)
+	
+	e = c + d 
+
+	m.A = strconv.Itoa(e)
+	m.codeindex += 2
 }
 
-func (m *machine) je(a interface{}){
-	val := a.(string)
+func (m *machine) sub(a string,b string){
+	c,_ := strconv.Atoi(a)
+	d,_ := strconv.Atoi(b)
+
+	e := c - d
+	m.A = strconv.Itoa(e)
+	
+	m.codeindex += 2
+}
+
+func (m *machine) mul(a string ,b string){
+	c,_ := strconv.Atoi(a)
+	d,_ := strconv.Atoi(b)
+
+	e := c * d
+	m.A = strconv.Itoa(e)
+
+	m.codeindex += 2
+}
+
+func (m *machine) div(a string,b string){
+	c,_ := strconv.Atoi(a)
+	d,_ := strconv.Atoi(b)
+
+	e := c / d
+	m.A = strconv.Itoa(e)
+
+	m.codeindex += 2
+}
+
+func (m *machine) mod(a string ,b string){
+	c,_ := strconv.Atoi(a)
+	d,_ := strconv.Atoi(b)
+
+	e := c % d
+	m.A = strconv.Itoa(e)
+
+	m.codeindex += 2
+}
+
+func (m *machine) cmp(a string,b string){
+	var av string
+	var bv string
+	
+	if a == "[X]"{
+		av = m.X
+	}else if a == "[Y]"{
+		av = m.Y
+	}else if a == "[A]"{
+		av = m.A
+	}else {
+		av = a
+	}
+
+	if b == "[X]"{
+		bv = m.X
+	}else if b == "[Y]"{
+		bv = m.Y
+	}else if b == "[A]"{
+		bv = m.A
+	}else{
+		bv = b
+	}
+
+	i,_ := strconv.Atoi(av)
+	j,_ := strconv.Atoi(bv)
+	
+	if j == i {
+		m.cmpf = true
+		m.greater = false
+		m.lesser = false
+	}else if  i > j {
+		
+		m.cmpf = false
+		m.greater = true
+		m.lesser = false
+		
+	}else if i < j {
+		
+		m.cmpf = false
+		m.greater = false
+		m.lesser = true
+	}
+
+	m.codeindex += 2
+
+}
+
+
+// JUMP IF EQUAL
+
+func (m *machine) je(val string){
 
 	if m.cmpf == true{
 		for _ , i := range m.labelarr{
@@ -201,15 +346,96 @@ func (m *machine) je(a interface{}){
 				break
 			}
 		}
+		m.codeindex++
+	}else{
+		m.codeindex += 2
 	}
+
+	
+	
+}
+
+//JUMP IF NOT EQUAL 
+
+func (m *machine) jne(val string){
+
+	if m.cmpf == false{
+		for _ , i := range m.labelarr{
+			if val == i.name{
+				m.codeindex = i.index
+				break
+			}
+		}
+		m.codeindex++
+	}else{
+		m.codeindex += 2
+	}
+	
+	
+	
+}
+
+// JUMP IF GREATER
+func (m *machine) jig(val string){
+
+	if m.greater == true{
+		for _ , i := range m.labelarr{
+			if val == i.name{
+				m.codeindex = i.index
+				break
+			}
+		}
+		m.codeindex++
+	}else{
+		m.codeindex += 2
+	}
+	
+	
+	
+}
+
+
+//JUMP IF LESSER
+func (m *machine) jil(val string){
+
+	if m.lesser == true{
+		for _ , i := range m.labelarr{
+			if val == i.name{
+				m.codeindex = i.index
+				break
+			}
+		}
+		m.codeindex++
+	}else{
+		m.codeindex += 2
+	}
+	
+	
 	
 }
 
 
 
+
+
 //@ this is a debug function and must be replaced
-func (m *machine) print(val interface{}){
-	fmt.Print(val)
+func (m *machine) print(val string){
+	if val == "[A]" {
+		fmt.Print(m.A)
+		
+	}else if val == "[X]" {
+		fmt.Print(m.X)
+		
+	}else if val == "[Y]" {
+		fmt.Print(m.Y)
+		
+	}else if val == "<newline>" {
+		fmt.Println()
+		
+	}else{
+		fmt.Print(val)
+	}
+	
 	m.codeindex += 1
 }
 //@ remove this !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -219,7 +445,7 @@ func (m *machine) print(val interface{}){
 func islabel(val string) bool{        //yes this also includes procs
 	i := []rune(val)
 
-	if i[0] == '_' || i[0] == '.'{
+	if i[0] == '#' || i[0] == '.'{
 		return true
 	}
 
